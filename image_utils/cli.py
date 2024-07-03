8994738eca93ea16d86ce182c73061751be50e93
kent.chen <kent.chen@hwacom.com>
2024-07-03 17:16:21 +0800

    #3 fix broken cli

--- image_utils/cli.py
+++ image_utils/cli.py
@@ -1,6 +1,11 @@
+from . import pipe
+from .decorators import common_options
+from .operators import crop as _crop, resize_with_pattern
+from PIL import Image
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
 import click
-from image_utils.decorators import common_options, handle_image_io
-from image_utils.operators import crop, resize_with_pattern
+import sys
 
 @click.group()
 def cli():
@@ -10,18 +15,41 @@ def cli():
 @cli.command()
 @click.argument('size')
 @common_options
-@handle_image_io(resize_with_pattern, 'size')
-def resize_image(size, input):
+def resize(size, input):
     """Resize an image"""
-    pass
+
+    image = fetch_image(input_path=input)
+    pipeline = pipe(resize_with_pattern(size))
+
+    pipeline.process(image).save(sys.stdout.buffer, format=image.format)
 
 @cli.command()
 @click.option('--align', type=click.Choice(['top', 'bottom', 'left', 'right', 'center']), default='center', help='Align the cropped image in the square (default: center)')
 @common_options
-@handle_image_io(crop, 'align')
-def crop_image(input, align):
+def crop(input, align):
     """Crop an image"""
-    pass
+
+    image = fetch_image(input_path=input)
+    pipeline = pipe(_crop(align))
+
+    pipeline.process(image).save(sys.stdout.buffer, format=image.format)
+
+def fetch_image(input_path: str) -> Image.Image:
+    # Load the input image from the provided path, stdin, or a file dialog
+    if input_path:
+        input_image = Image.open(input_path)
+    else:
+        if not sys.stdin.isatty():
+            input_image = Image.open(sys.stdin.buffer)
+        else:
+            Tk().withdraw()
+            input_path = askopenfilename(title="Select an image to process")
+            if not input_path:
+                print("No input file provided. Exiting.")
+                sys.exit(1)
+            input_image = Image.open(input_path)
+    
+    return input_image
 
 if __name__ == "__main__":
     cli()
--- image_utils/decorators/__init__.py
+++ image_utils/decorators/__init__.py
@@ -1,4 +1,3 @@
 from .common_options import common_options
-from .handle_image_io import handle_image_io
 
-__all__ = ['common_options', 'handle_image_io']
+__all__ = ['common_options']
