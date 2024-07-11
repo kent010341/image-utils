9daaf5d9d664afff2f48b965b8d1dec8178076c5
kent.chen <kent.chen@hwacom.com>
2024-07-11 16:53:37 +0800

    #13 fix wrong spelling

--- image_utils/operators/__init__.py
+++ image_utils/operators/__init__.py
@@ -1,5 +1,5 @@
 from .crop_operator import crop
 from .resize_operator import resize, resize_with_pattern
-from .grap_scale_operator import gray_scale
+from .gray_scale_operator import gray_scale
 
 __all__ = ['crop', 'resize', 'resize_with_pattern', 'gray_scale']
